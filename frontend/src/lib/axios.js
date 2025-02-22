b996f777ded0d340cbf21d052a3cec570de35774
Karan Jalal <karanjalal1109@gmail.com>
2025-02-22 19:34:30 +0530

    updated axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // Create axios instance
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/",
+    baseURL: "https://spendwise-f400.onrender.com/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
