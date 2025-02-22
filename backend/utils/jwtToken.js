ed91ca8dd4b4499f299309a535d7fcdbcf97391f
Karan Jalal <karanjalal1109@gmail.com>
2025-02-22 19:43:37 +0530

    jwt updated

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,7 +9,7 @@ export const generateToken= (userId, res)=>{
         maxAge: 7*24*60*60*1000,  //it is in milliseconds
         httpOnly: true,  //it prevents cross site scripting attacks
         sameSite: "none",
-        secure:  process.env.NODE_ENV === "production",
+        secure: true,
     })
 
     return token
