047aa2294fd18bf520d976f6cd9ab1a9f61b1f0c
Karan Jalal <karanjalal1109@gmail.com>
2025-02-22 20:25:27 +0530

    updated

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,7 @@ export const generateToken= (userId, res)=>{
     res.cookie("jwt",token,{
         maxAge: 7*24*60*60*1000,  //it is in milliseconds
         httpOnly: true,  //it prevents cross site scripting attacks
-        sameSite: "none",
+        sameSite: "None",
         secure:  process.env.NODE_ENV === "production",
     })
 
